211ccdfd4461dbc2551217a4c333f23d5094c72a
DahamLeee <dlek1011@gmail.com>
2022-08-09 00:34:26 +0900

    test

--- java-racingcar/src/main/java/calculator/StringAddCalculator.java
+++ java-racingcar/src/main/java/calculator/StringAddCalculator.java
@@ -7,10 +7,11 @@ public class StringAddCalculator {
 
     private static final String DEFAULT_DELIMITER = ",|:";
     private static final String CUSTOM_DELIMITER_REGEXP = "//(.)\n(.*)";
+    private static final int ZERO = 0;
 
     public static int splitAndSum(String text) {
         if (isBlank(text)) {
-            return 0;
+            return ZERO;
         }
 
         return totalSum(splitText(text));
